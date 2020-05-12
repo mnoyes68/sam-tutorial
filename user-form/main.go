96b59113c2468aedf729d29cdecb553438a5c1b8
Matthew Noyes <noyem@amazon.com>
2020-05-12 17:34:50 -0400

    Converted to ShowForm

---
+++ user-form/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"log"
+	"net/http"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+var (
+	// DefaultHTTPGetAddress Default Address
+	DefaultHTTPGetAddress = "https://checkip.amazonaws.com"
+
+	// ErrNon200Response non 200 status code in response
+	ErrNon200Response = errors.New("Non 200 Response found")
+
+	HTMLPageBody =`
+  <html>
+  <head>
+    <meta charset="utf-8"/>
+  </head>
+  <body>
+    <form method="POST">
+      Please enter your name:
+      <input type="text" name="name"/>
+      <br/>
+      <input type="submit" />
+    </form>
+  </body>
+  </html>
+`
+)
+
+func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+    // Log the request
+	requestJSON, _ := json.Marshal(request)
+	log.Println(string(requestJSON))
+
+	resp, err := http.Get(DefaultHTTPGetAddress)
+	if err != nil {
+		return events.APIGatewayProxyResponse{}, err
+	}
+
+	if resp.StatusCode != 200 {
+		return events.APIGatewayProxyResponse{}, ErrNon200Response
+	}
+
+	return events.APIGatewayProxyResponse{
+		Body: HTMLPageBody,
+		StatusCode: 200,
+		Headers: map[string]string{
+			"Content-Type": "text/html",
+		},
+	}, nil
+}
+
+func main() {
+	lambda.Start(handler)
+}
